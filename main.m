f1ce0d3cc21a26cce155b2cda67a8789888c69e4
joshuammueller <joshua.m.mueller@gmail.com>
2015-04-03 13:26:12 -0400

    function shells
    Made some basic function shells to build on. I’ll continue to play with
    this today.

--- main.m
+++ main.m
@@ -2,3 +2,41 @@
 % Authors: Paul Natsuo Kishimoto <pnk@MIT.EDU>
 %          Joshua Michael Mueller <jmmuell@MIT.EDU>
 % Household Storage Requirements
+
+
+function household
+% This funcion calls the functions which generate the stochastic inputs.
+
+hours = 1:24*14;
+
+
+
+
+netdemand = demand .- generation;
+
+end
+
+
+function [demand] = makedemand(hours)
+% This function generates the stochastic demand profile for the household.
+demand = zeros(length(hours)); %make basic demand matrix
+demanderror = zeros(length(hours)); %make the error matrix that will sample from the distribution
+end
+
+
+function [gen] = makegeneration(hours)
+%This function generates the stochastic wind generation profile. First it
+%makes the stochastic wind speed then it converts the wind speed to a
+%power.
+speed = zeros(length(hours));
+
+generation = speed.^3; %convert speed stochastic data to generation
+end
+
+function [prices] = inputprices(hours)
+%This function generates the stochastic price data seen by the household.
+prices = zeros(length(hours)); %make price matrix
+priceserror = zeros(length(hours)); %make the stochastic matrix for the price data
+
+
+end
