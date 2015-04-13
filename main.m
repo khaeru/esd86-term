e4a8feaae592209498247f20540658c4795f9009
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-13 10:59:35 -0400

    Debug

--- main.m
+++ main.m
@@ -4,9 +4,19 @@
 
 % Household Storage Requirements
 
-function montecarlo
-  N = 500;
-  costs = zeros([1 N])
+function main(cmd)
+  switch cmd
+    case 'mc'
+      montecarlo(500)
+  
+    case 'single'
+      household(true)
+  end
+end
+
+
+function montecarlo(N)
+  costs = zeros([1 N]);
   for draw = 1:N
     costs(draw) = household;
   end
@@ -15,35 +25,38 @@ function montecarlo
   saveas(gcf, 'costs_mc.svg');
 end
 
-function [totalcost] = household(plot)
+
+function [totalcost] = household(save_plots)
   % This funcion calls the functions which generate the stochastic inputs for
   % a two week period.
 
   if nargin < 1
-    plot = false
+    save_plots = false;
   end
   
   days = 7;
-  hours = 1:24 * days;
+  hours = 1:(24 * days);
   demandmeanerror = 0; %For adjusting the demand stochastic normal distribution
   demandstddev = 1; % for adjusting the demand stochastic normal distribution
   pricesmeanerror = 0;% for adjusting the prices stochastic normal distribution
   pricesstddev = 1; % for adjusting the prices stochastic normal distribution
   genshape = 1; %for adjusting the speed weibull distribution
-  genscale = 1; %for adjusting the speed weibull distribution
+  genscale = 0.5; %for adjusting the speed weibull distribution
 
-  [demand] = makedemand(hours,demandmeanerror,demandstddev);
-  [generation] = makegeneration(hours,genshape,genscale);
-  [prices] = inputprices(hours,pricesmeanerror,pricesstddev);
+  [demand] = makedemand(hours, demandmeanerror, demandstddev);
+  [generation] = makegeneration(hours, genshape, genscale);
+  [prices] = inputprices(hours, pricesmeanerror, pricesstddev);
 
   % Compute net demand
   netdemand = demand - generation;
 
+  % TODO identify & integrate negative-demand hours -> battery storage
+  
   % Compute electricity cost
-  cost = netdemand .* prices;
-  totalcost = sum(cost)
+  cost = min(zeros(size(hours)), netdemand) .* prices;
+  totalcost = sum(cost);
 
-  if logical(plot)
+  if logical(save_plots)
     figure;
     plot(hours, demand, 'b', hours, generation, 'g', hours, netdemand, 'r');
     xlabel('Hours')
@@ -61,15 +74,14 @@ function [totalcost] = household(plot)
   end
 end
 
-function [demand] = makedemand(hours,mean,stddev)
+
+function [demand] = makedemand(hours, mean, stddev)
   % This function generates the stochastic demand profile for the household.
   scale = 10;
-  demand = zeros([1 length(hours)]); %make basic demand matrix
   % Make the error matrix that will sample from a standard normal distribution
-  demanderror = zeros([1 length(hours)]);
   daily = [3 3 3 3 3 4 5 7 7 5 5 5 4 4 5 7 8 9 8 6 6 5 4 3];
   base = repmat(daily, 1, length(hours) / 24) * scale;
-  demanderror = randn([1 length(hours)])*stddev + mean;
+  demanderror = randn([1 length(hours)]) * stddev + mean;
   demand = base + demanderror;
 end
 
@@ -78,21 +90,20 @@ function [generation] = makegeneration(hours, shape, scale)
   % This function generates the stochastic wind generation profile. First it
   % makes the stochastic wind speed then it converts the wind speed to a
   % power.
-  scale = 8;
+  scale2 = 10;
+  maxgen = 50 * ones(size(hours));
   % Conditional distribution: low, medium or high wind days
   %wind = ceil(rand * 3);
   wind = rand * 3;
-  speed = wblrnd(shape, scale, [1 length(hours)]);
+  speed = wblrnd(shape, scale , [1 length(hours)]);
   % Convert speed stochastic data to generation - not yet correct
-  generation = wind .* scale .* speed .^ 3;
+  generation = min(maxgen, wind .* scale2 .* (speed .^ 3));
 end
+
 
 function [prices] = inputprices(hours, mean, stddev)
   % This function generates the stochastic price data seen by the household.
-  prices = zeros([1 length(hours)]); %make price matrix
   % Make the stochastic matrix for the price data
-  priceserror = zeros([1 length(hours)]);
-  base = prices;
   base = sin(pi * hours / 12 + pi) + 5; % makes the basic sinusoid of prices
   priceserror = randn([1 length(hours)]) * stddev + mean;
   prices = base + priceserror;
